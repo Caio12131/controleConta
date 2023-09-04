6f6ec88b4e266650e1d4e8c34d6a93d36edc2997
caioc <caioc@PREDATORCAIO>
2023-09-04 19:50:21 -0300

    Adicionar arquivos de projeto.

---
+++ ControleContas/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace controleContas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta1 = new Conta();
+            conta1.Numero = 123456;
+            Console.WriteLine(conta1.Numero);
+        }
+
+    }
+}    
